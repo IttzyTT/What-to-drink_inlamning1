bdfa359d553bcc35ab6d73be13b8dc90232c868b
Igor Ljevak <igorljevak@gmail.com>
2020-11-25 17:41:48 +0100

    style correction

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-async function fetchData() {
+async function fetchData('https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free') {
   try {
     let response = await fetch();
     let data = await response.json();
